bc33e4bf6b3771a049d797420de52bb3a99c9c4d
Kongmeng Xiong <kongmengxiong479@gmail.com>
2023-08-03 17:35:37 -0500

    Updated Addblog.js

--- admin-app/src/pages/Addblog.js
+++ admin-app/src/pages/Addblog.js
@@ -1,24 +1,35 @@
 import React, { useState } from 'react'
 import CustomInput from '../components/CustomInput'
-
-
-
-const [desc, setDesc] = useState();
-const handleDesc = (e) => {
-    console.log(e);
-}
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { Stepper } from 'react-form-stepper';
 
 const Addblog = () => {
+
+    const [desc, setDesc] = useState();
+    const handleDesc = (e) => {
+        setDesc(e);
+}
+
   return (
     <div>
         <h3 className='mb-4'>Add Blog</h3>
+        <Stepper
+            steps={[{ label: 'Add Blog' }, { label: 'Upload Images' }, { label: 'Finish' }]}
+            activeStep={0}
+        />
         <div className=''>
             <form action=''>
                 <CustomInput type='text' label='Enter Blog Title' />
-                    <select name='' id=''>
+                    <select name='' className='form-control py-3 mb-3' id=''>
                         <option value=''>Select Blog Category</option>
                     </select>
-
+                    <ReactQuill 
+                        theme="snow" 
+                        value={desc} 
+                        onChange={(evt) => {handleDesc(evt)}} 
+                    />
+                    <button className="btn btn-success border-0 rounded-3 my-5" type="submit">Add Blog</button>
             </form>
         </div>
     </div>
