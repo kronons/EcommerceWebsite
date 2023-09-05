97dcf1a465b3d6c8fd62294211fa2e2cb707601c
Kongmeng Xiong <kongmengxiong479@gmail.com>
2023-09-05 15:18:56 -0500

    Added register user functionality to front end

--- client/src/components/CustomInput.js
+++ client/src/components/CustomInput.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 export const CustomInput = (props) => {
-    const {type, name, placeholder, className} = props
+    const {type, name, placeholder, className, value, onChange, onBlur} = props
 
   return (
     <div>
         <input 
-            type={type}
-            name={name}
-            placeholder={placeholder}
-            className={`form-control ${className}`}
+            type = {type}
+            name = {name}
+            placeholder = {placeholder}
+            className = {`form-control ${className}`}
+            value = {value}
+            onChange = {onChange}
+            onBlur = {onBlur}
         />
     </div>
   )
