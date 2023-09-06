ac3e03e8f1babec1d830a4398a207059a25ae103
Kongmeng Xiong <kongmengxiong479@gmail.com>
2023-09-06 11:15:16 -0500

    Implemented login functionality

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -10,7 +10,17 @@ export const registerUser = createAsyncThunk("auth/register", async ( userData, 
         return thunkAPI.rejectWithValue(error);
     }
     
-})
+});
+
+export const loginUser = createAsyncThunk("auth/login", async ( userData, thunkAPI ) => {
+    try{
+        return await authService.login(userData);
+    }
+    catch(error) {
+        return thunkAPI.rejectWithValue(error);
+    }
+    
+});
 
 const initialState = {
     user: "",
@@ -39,6 +49,28 @@ export const authslice = createSlice({
             }
         })
         .addCase(registerUser.rejected, ( state, action ) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.isSuccess = false;
+            state.message = action.error;
+            if(state.isError === true) {
+                toast.error(action.error);
+            }
+        })
+        .addCase(loginUser.pending, ( state ) => {
+            state.isLoading = true;
+        })
+        .addCase(loginUser.fulfilled, ( state, action ) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.loginUser = action.payload;
+            if(state.isSuccess === true) {
+                localStorage.setItem("token", action.payload.token);
+                toast.info("User Logged In Successfully");
+            }
+        })
+        .addCase(loginUser.rejected, ( state, action ) => {
             state.isLoading = false;
             state.isError = true;
             state.isSuccess = false;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,8 +4,33 @@ import Meta from '../components/Meta'
 import { Link } from 'react-router-dom'
 import Container from '../components/Container'
 import { CustomInput } from '../components/CustomInput'
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../features/user/userSlice'
+
+
+const loginSchema = Yup.object({
+    email: Yup.string().email('Invalid email').required('Email is Required'),
+    password: Yup.string().required("Password is Required")
+  })
+  
 
 const Login = () => {
+
+    const dispatch = useDispatch();
+
+    const formik = useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: loginSchema,
+      onSubmit: ( values ) => {
+        dispatch(loginUser(values));
+      },
+    });
+
   return (
     <>
         <Meta title={'Login'}/>
@@ -16,10 +41,39 @@ const Login = () => {
                     <div className='col-12'>
                         <div className='auth-card'>
                             <h3 className='text-center mb-3'>Login</h3>
-                            <form action='' className='d-flex flex-column gap-15'>
-                                <CustomInput type='email' name='email' placeholder='Email' />
-                                <CustomInput type='password' name='password' placeholder='Password'  />
+                            <form action='' onSubmit={formik.handleSubmit} className='d-flex flex-column gap-15'>
+
+                                <CustomInput 
+                                    type = 'email' 
+                                    name = 'email' 
+                                    placeholder = 'Email' 
+                                    onChange = { formik.handleChange("email") }
+                                    onBlur = { formik.handleBlur("email") }
+                                    value = { formik.values.email}
+                                />
+
                                 
+                                <div className='error'>
+                                    {
+                                        formik.touched.email && formik.errors.email
+                                    }
+                                </div>
+
+                                <CustomInput 
+                                    type='password' 
+                                    name='password' 
+                                    placeholder='Password'  
+                                    onChange = { formik.handleChange("password") }
+                                    onBlur = { formik.handleBlur("password") }
+                                    value = { formik.values.password}
+                                />
+
+                                <div className='error'>
+                                    {
+                                        formik.touched.password && formik.errors.password
+                                    }
+                                </div>
+
                                 <div>
                                     <Link to='/forgot-password'>Forgot Password?</Link>
                                     <div className=' mt-3 d-flex justify-content-center gap-15 align-items-center'>
